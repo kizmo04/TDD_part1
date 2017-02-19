3d66f0967157dd1b846865dd72fbde70b77dcf5b
kizmo04 <kizmo04@gmail.com>
2017-02-19 21:03:27 +0900

    POST후에 리디렉션과 템플릿에 모든 작업 아이템 표시, 기능 테스트 주석 추가

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Item
 
 
 def home_page(request):
-    # if request.method == 'POST':
-    #     return HttpResponse(request.POST['item_text'])
-    return render(request, 'lists/home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
-    })
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    items = Item.objects.all()
+    return render(request, 'lists/home.html', {'items': items})
+
+    # else:
+    #     new_item_text = ''
+
+    # return HttpResponse(request.POST['item_text'])
+    # item = Item()
+    # item.text = request.POST.get('item_text', '')
+    # item.save()
+
+    # return render(request, 'lists/home.html', {
+    #     'new_item_text': new_item_text,
+    # })
